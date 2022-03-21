6c2ef52596f77d9e1628c200b7731610f77dd859
Jayendran-R <101970980+Jayendran-R@users.noreply.github.com>
2022-03-21 16:13:11 +0530

    Add files via upload

---
+++ ConvertNegToPos.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class ConvertNegToPos {
+
+	public static void main(String[] args) {
+		int number = -40;
+		if (number<0) {
+			
+			number = (number+-2*number);
+		} 
+		System.out.println("The number -40 is converted to 40");
+
+	}
+
+}
